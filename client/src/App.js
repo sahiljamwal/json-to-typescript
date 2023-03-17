2bbbda7e0bfec884219da06f10802f30fbf10251
sahil-jamwal <sahil.jamwal78625@gmail.com>
2023-03-17 23:03:34 +0530

    update : master | added ui for code editor

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,61 @@
-import "./App.css";
+import React, { useState } from "react";
+import Copy from "./icons/Copy.icons";
+import Delete from "./icons/Delete.icons";
+import Editor from "@monaco-editor/react";
 
-function App() {
-  return <div className="App">hello world</div>;
-}
+const App = () => {
+  const [value, setValue] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Run button clicked");
+  };
+
+  return (
+    <main className="app">
+      <header className="header__container">
+        <div className="header">
+          <h3>JSON</h3>
+          <div className="header__right">
+            <button className="runBtn" onClick={handleSubmit}>
+              RUN
+            </button>
+            <Delete />
+          </div>
+        </div>
+        <div>
+          <h3>Typescript</h3>
+          <Copy />
+        </div>
+      </header>
+      <div className="code__container">
+        <div className="code">
+          <Editor
+            height="90vh"
+            className="editor"
+            defaultLanguage="json"
+            defaultValue="{}"
+            value={value}
+            onChange={(value) => setValue(value)}
+          />
+        </div>
+        <div className="output">
+          <Editor
+            height="90vh"
+            className="editor"
+            defaultLanguage="typescript"
+            defaultValue=""
+            options={{
+              domReadOnly: true,
+              readOnly: true,
+            }}
+            value={output}
+            onChange={(value) => setOutput(value)}
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default App;
